42036e455cf69835d492b21d3ee99218965ad8ec
NovikFeed <vania200345@gmail.com>
2024-09-08 20:31:01 +0300

    Add mapers for map type data

---
+++ app/src/main/java/com/example/catty/personList/data/mapers/Mapers.kt
@@ -0,0 +1,30 @@
+package com.example.catty.personList.data.mapers
+
+import com.example.catty.personList.data.local.UserEntity
+import com.example.catty.personList.data.remote.respond.UserDTO
+import com.example.catty.personList.domain.model.User
+
+fun UserDTO.toEntity():UserEntity{
+    return UserEntity(
+        email = email ?: "",
+        id = id ?: 0,
+        name = name ?: "",
+        phone = phone ?: "",
+        photo = photo ?: "",
+        position = position ?: "",
+        position_id = position_id ?: 0,
+        registration_timestamp = registration_timestamp ?: 0
+    )
+}
+fun UserDTO.toUser() : User {
+    return User(
+        email = email ?: "",
+        id = id ?: 0,
+        name = name ?: "",
+        phone = phone ?: "",
+        photo = photo ?: "",
+        position = position ?: "",
+        position_id = position_id ?: 0,
+        registration_timestamp = registration_timestamp ?: 0
+    )
+}
