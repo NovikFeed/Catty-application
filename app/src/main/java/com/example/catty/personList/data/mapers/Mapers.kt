076d3bcf485703b14561707f3916f738a8be29ed
ivanovich <vania200345@gmail.com>
2024-09-09 10:27:04 +0300

    Add map method from userEntity to User

--- app/src/main/java/com/example/catty/personList/data/mapers/Mapers.kt
+++ app/src/main/java/com/example/catty/personList/data/mapers/Mapers.kt
@@ -4,7 +4,7 @@ import com.example.catty.personList.data.local.UserEntity
 import com.example.catty.personList.data.remote.respond.UserDTO
 import com.example.catty.personList.domain.model.User
 
-fun UserDTO.toEntity():UserEntity{
+fun UserDTO.toEntity(): UserEntity {
     return UserEntity(
         email = email ?: "",
         id = id ?: 0,
@@ -16,7 +16,8 @@ fun UserDTO.toEntity():UserEntity{
         registration_timestamp = registration_timestamp ?: 0
     )
 }
-fun UserDTO.toUser() : User {
+
+fun UserDTO.toUser(): User {
     return User(
         email = email ?: "",
         id = id ?: 0,
@@ -26,5 +27,18 @@ fun UserDTO.toUser() : User {
         position = position ?: "",
         position_id = position_id ?: 0,
         registration_timestamp = registration_timestamp ?: 0
+    )
+}
+
+fun UserEntity.toUser(): User {
+    return User(
+        email = email,
+        id = id,
+        name = name,
+        phone = phone,
+        photo = photo,
+        position = position,
+        position_id = position_id,
+        registration_timestamp = registration_timestamp
     )
 }
