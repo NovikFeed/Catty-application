32d44ebabfc725203f7417566165f08bc54e0062
NovikFeed <vania200345@gmail.com>
2024-09-11 17:20:55 +0300

    Add dependency for coil lib for work with async image painter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,8 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.51.1")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
 
 
